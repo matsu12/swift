4cd26b675728a34f8946023cc9b5345076ceacea
matsu12 <dai1230@matsuuradaiki-no-MacBook-Air.local>
2015-07-07 13:30:43 +0900

    first commit

---
+++ tumblr/CellViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CellViewController.swift
+//  tumblr
+//
+//  Created by matsuuradaiki on 2015/07/03.
+//  Copyright (c) 2015年 matsuuradaiki. All rights reserved.
+//
+
+import UIKit
+
+class CellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
